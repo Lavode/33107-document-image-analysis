497ff1f7b5e64af47e7f08e2d2ecd9507ecb551d
Michael Senn <michael@morrolan.ch>
2021-03-20 17:53:44 +0100

    Generalize features to be used by classifier.

--- exercises/02_classification/classification.py
+++ exercises/02_classification/classification.py
@@ -37,19 +37,28 @@ CLASSES_COUNT = len(CLASSES)
 class Classifier:
     feature_count = 2
 
-    def __init__(self, classes, max_sample_count):
+    def __init__(self, classes, max_sample_count, features):
         self.classes = classes
         self.max_sample_count = max_sample_count
 
-        # class => (sample_count, 28) np array
-        self.horizontal_profile = {}
+        self.features = features
 
-        # class => (sample_count, 28) np array
-        self.vertical_profile = {}
-
+        # class => feature id => np array
+        self.samples = {}
         for cls in classes:
-            self.horizontal_profile[cls] = np.zeros((max_sample_count, 28))
-            self.vertical_profile[cls] = np.zeros((max_sample_count, 28))
+            print("Initializing features of class {}".format(cls))
+            self.samples[cls] = {}
+            for feature in features:
+                dim = (max_sample_count, ) + feature['dim']
+                print("\tInitializing feature '{}' with dimension {}".format(feature['name'], dim))
+                self.samples[cls][feature['id']] = np.zeros(dim)
+
+        # class => feature id => np array
+        self.representatives = {}
+        for cls in classes:
+            self.representatives[cls] = {}
+            for feature in features:
+                self.representatives[cls][feature['id']] = np.zeros(feature['dim'])
 
         # class => Maximum index of currently unused place in corresponding sample np array
         self.current_idx = {}
@@ -62,48 +71,57 @@ class Classifier:
 
         idx = self.current_idx[cls]
 
-        self.horizontal_profile[cls][idx] = self.extract_horizontal_profile(pixels)
-        self.vertical_profile[cls][idx] = self.extract_vertical_profile(pixels)
+        for feature in self.features:
+            self.samples[cls][feature['id']][idx] = feature['f'](pixels)
 
         self.current_idx[cls] += 1
-
-    def extract_horizontal_profile(self, pixels):
-        return self._profile_along_axis(pixels, 0)
-
-    def extract_vertical_profile(self, pixels):
-        return self._profile_along_axis(pixels, 1)
 
     def finalize(self):
         print("Finalizing classifier")
 
-        print("Truncating unused samples")
         for cls in self.classes:
+            print("Truncating unused samples")
             allocated = self.max_sample_count
             used = self.current_idx[cls]
 
-            print("\tClass {} has {} samples allocated, {} used. Truncating...".format(cls, allocated, used))
-            # `used` is highest *unused* index, so is also the number of
-            # trained samples - hence the slicing just works
-            self.horizontal_profile[cls] = self.horizontal_profile[cls][:used, :]
-            self.vertical_profile[cls] = self.vertical_profile[cls][:used, :]
+            for feature in self.features:
+                feature_id = feature['id']
 
-        print("Calculating representative as average of used features")
+                print("\tClass {} has {} samples allocated, {} used. Truncating...".format(cls, allocated, used))
+                # `used` is highest *unused* index, so is also the number of
+                # trained samples - hence the slicing just works
+                self.samples[cls][feature_id] = self.samples[cls][feature_id][: used, :]
 
-    def _profile_along_axis(self, pixels, axis):
-        # Data we get is grayscale so in [0, 256). To ensure the profile is in
-        # this range too, we normalize by dividing by the shape along this
-        # axis.
-        return (pixels / pixels.shape[axis]).sum(axis).astype(np.uint8)
+                print("Calculating representative as mean of used features")
+                # Sum along sample axis (0), then divide by number of samples
+                self.representatives[cls][feature_id] = (self.samples[cls][feature_id].sum(0) / used).astype(np.uint8)
+
 
 
 def main():
-    classifier = Classifier(CLASSES, TRAINING_DATA_COUNT)
+    features = [
+            {
+                'id': 'horizontal_profile',
+                'name': "Horizontal profile",
+                'f': extract_horizontal_profile,
+                'dim': (28,)
+            },
+            {
+                'id': 'vertical_profile',
+                'name': "Vertical profile",
+                'f': extract_vertical_profile,
+                'dim': (28,)
+            },
+    ]
+    classifier = Classifier(CLASSES, TRAINING_DATA_COUNT, features)
 
     # pixels = load_img('mnist/train/0/21273.png', rgb=False)
     # classifier.train_img(pixels, '0')
 
     train_classifier_from_data(classifier)
     classifier.finalize()
+
+    print(classifier.representatives['3']['vertical_profile'])
 
 def train_classifier_from_data(classifier):
     for cls in CLASSES:
@@ -122,6 +140,19 @@ def train_classifier_from_data(classifier):
 
             if cnt >= MAX_SAMPLE_COUNT_PER_CLASS:
                 break
+
+def extract_horizontal_profile(pixels):
+    return profile_along_axis(pixels, 0)
+
+def extract_vertical_profile(pixels):
+    return profile_along_axis(pixels, 1)
+
+def profile_along_axis(pixels, axis):
+    # Data we get is grayscale so in [0, 256). To ensure the profile is in
+    # this range too, we normalize by dividing by the shape along this
+    # axis.
+    return (pixels / pixels.shape[axis]).sum(axis).astype(np.uint8)
+
 
 # Loads image using CV2, returns a n*m*3 (for RGB) respectively a n*m (for
 # grayscale) numpy array of pixel values.
